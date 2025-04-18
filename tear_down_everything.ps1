5d44a359bb2295e5dc49e43c37a835f265e7e5c8
Max <48467432+Max-Frisch@users.noreply.github.com>
2025-04-18 14:54:44 +0700

    added 'remove all resources' -script

---
+++ tear_down_everything.ps1
@@ -0,0 +1,18 @@
+
+# Saving ALL resourcegroups in this Subscription into an object of resourcegroups
+$AllResourceGroups = Get-AzResourceGroup
+
+# Looping over ALL resource groups($AllResourceGroups), removing the RSGs and all created 
+# resources inside them. Basically a complete clean-up to cut down costs!
+# As this Azure tenant is used for learning and practice, no "production"-resources are touched
+foreach ($Group in $AllResourceGroups) {
+        Write-Host "Removing Resource Group: $($Group.ResourceGroupName).."
+        Remove-AzResourceGroup -Name $Group.ResourceGroupName -Force
+}
+
+# Check if all resource groups and resources have been deleted, or not!
+if (! $(Get-AzResourceGroup) -or ! $(Get-AzResource)) {
+    Write-Host -ForegroundColor Green "All resource groups including all resources are deleted."
+} else {
+    Write-Host -ForegroundColor Red "Some resource groups or resources have not been deleted! Check manually!!"
+}
